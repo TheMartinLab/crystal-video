36e9ed4e3e4f9dd9ccf1a583618302b489f2601d
Eric Dill <thedizzle@gmail.com>
2022-08-08 17:56:37 -0400

    Almost have drawing on the scrubber working

--- some_math/__init__.py
+++ some_math/__init__.py
@@ -7,7 +7,7 @@ import string
 import random
 import numpy as np
 from ipywidgets import widgets
-from traitlets import Float, HasTraits
+import traitlets
 from typing import Tuple
 import random_word
 import pathlib
@@ -23,8 +23,8 @@ def word_pair():
     return f'{random.choice(WORDS)} {random.choice(compounds)}'
 
 class Point(widgets.Text):
-    x = Float()
-    y = Float()
+    x = traitlets.Float()
+    y = traitlets.Float()
     
     def __init__(self, x, y, *args, **kwargs):
         super(Point, self).__init__(*args, **kwargs)
@@ -47,8 +47,9 @@ _LETTERS_AND_DIGITS = string.ascii_lowercase[:6] + string.digits
 def random_hex_color():
     return '#' + ''.join(random.choices(_LETTERS_AND_DIGITS, k=6))
 
-class LinearFace(HasTraits):
-    rect_angle = Float()
+class LinearFace(traitlets.HasTraits):
+    rect_angle = traitlets.Float()
+    callback = traitlets.Any()
     
     def __init__(self, x1=0, y1=0, x2=1, y2=1, length=10, width=2):
         self.p1 = Point(0,0, description="p0")
@@ -57,14 +58,14 @@ class LinearFace(HasTraits):
         self.width = widgets.Text(description="Width", value=str(width))
         # self.midpoint = some_math.Point(*some_math.midpoint(self.p1, self.p2), description="midpoint")
         # self.rect_origin = some_math.Point(0,0, description="rect origin")
-        self.midpoint = Point(0,0, description="midpoint")
-        self.rect_origin = Point(0,0, description="rect origin")
-        self.widg_rect_angle = widgets.Label(value="")
+        self.midpoint = Point(0,0, description="midpoint", disabled=True)
+        self.rect_origin = Point(0,0, description="rect origin", disabled=True)
+        self.widg_rect_angle = widgets.Text("", description="rot (deg)", disabled=True)
         self.color_picker = widgets.ColorPicker(
             description='line',
             value=random_hex_color()
         )
-        self.name = widgets.Text(description="name", value=word_pair())
+        self.name = widgets.Text(description="name", value=word_pair(), disabled=True)
         
         recompute = [self.p1, self.p2, self.length, self.width]
         self.redraw = recompute + [self.color_picker]
@@ -88,12 +89,14 @@ class LinearFace(HasTraits):
         self.midpoint.x, self.midpoint.y = midpoint(self.p1, self.p2)
         # origin of rectangle should be "upstream" of the midpoint, so we reverse the point vector
         r, th = to_polar(self.p2, self.p1)
-        self.rect_angle = th
+        self.rect_angle = th * 180 / np.pi
         x, y = distance_polar(self.midpoint, float(self.width.value), th)
         self.rect_origin.x, self.rect_origin.y = x,y
+        if self.callback is not None:
+            self.callback(self)
     
     def _update_rect_angle(self, change):
-        self.widg_rect_angle.value = f'rectangle rotation: {self.rect_angle} rad'
+        self.widg_rect_angle.value = f'{self.rect_angle}'
     
         
 #     def _update_rectangle(self, change):
