2e0ec112664bf908b6f64aab420a047ac45aa13e
Eric Dill <ericdill@pm.me>
2022-08-09 14:16:34 -0400

    Still working

--- some_math/__init__.py
+++ some_math/__init__.py
@@ -1,4 +1,3 @@
-
 from importlib.machinery import WindowsRegistryFinder
 from importlib.resources import path
 from sys import float_info
@@ -11,100 +10,151 @@ import traitlets
 from typing import Tuple
 import random_word
 import pathlib
+import matplotlib.patches as patches
+import matplotlib as mpl
 
 WORDS = []
-compounds = ['CZX-1', 'ZnCl2', 'CuCl', 'halozeotype', 'amine', 'hydrate']
+compounds = ["CZX-1", "ZnCl2", "CuCl", "halozeotype", "amine", "hydrate"]
+
 
 def word_pair():
     global WORDS
     if not WORDS:
-        p = pathlib.Path(__file__).absolute().parent.parent / 'english-adjectives.txt'
-        WORDS = open(p, 'r').read().split('\n')
-    return f'{random.choice(WORDS)} {random.choice(compounds)}'
+        p = pathlib.Path(__file__).absolute().parent.parent / "english-adjectives.txt"
+        WORDS = open(p, "r").read().split("\n")
+    return f"{random.choice(WORDS)} {random.choice(compounds)}"
+
 
 class Point(widgets.Text):
     x = traitlets.Float()
     y = traitlets.Float()
-    
+
     def __init__(self, x, y, *args, **kwargs):
-        super(Point, self).__init__(*args, **kwargs)
+        super(Point, self).__init__(continuous_update=False, *args, **kwargs)
         self.x = x
         self.y = y
-        self.observe(self._update, names=['x', 'y', 'value'])
-        self.value = f'{x},{y}'
-        
-        
+        self.observe(self._update, names=["x", "y", "value"])
+        self.value = f"{x},{y}"
+
     def _update(self, change):
         print(change)
-        if change['name'] == 'value':
-            x,y = self.value.split(',')
-            self.x = float(x)
-            self.y = float(y)
-        if change['name'] == 'x' or change['name'] == 'y':
-            self.value = f'{self.x},{self.y}'
+        if change["name"] == "value":
+            x, y = self.value.split(",")
+            with self.hold_trait_notifications():
+                self.x = float(x)
+                self.y = float(y)
+        if change["name"] == "x" or change["name"] == "y":
+            self.value = f"{self.x},{self.y}"
+
+
+class Point_try2(widgets.HBox):
+    x = traitlets.Float()
+    y = traitlets.Float()
+
+    def __init__(self, x, y, *args, **kwargs):
+        super(Point, self).__init__(*args, **kwargs)
+        # self.x = x
+        # self.y = y
+        self.text_x = widgets.Text()
+        self.text_y = widgets.Text()
+        # for obj in [self.x, self.y, self.text_x, self.text_y]:
+        #     obj.observe(self._update)
+        # self.text_x.observe(self._update, names=['value'])
+        self.observe(self._update, names=["x", "y"])
+        self.text_x.observe(self._update_x, names=["value"])
+        self.text_y.observe(self._update, names=["value"])
+        self.children = [self.text_x, self.text_y]
+        # self.value = f"{x},{y}"
+
+    def _update(self, change):
+        print(change)
+        if change["name"] == "x":
+            self.text_x.value = str(change["new"])
+        elif change["name"] == "y":
+            self.text_y.value = str(change["new"])
+        elif change["name"] == "text_x":
+            self.x = change["new"]
+        elif change["name"] == "text_y":
+            self.y = change["new"]
+
 
 _LETTERS_AND_DIGITS = string.ascii_lowercase[:6] + string.digits
+
+
 def random_hex_color():
-    return '#' + ''.join(random.choices(_LETTERS_AND_DIGITS, k=6))
+    return "#" + "".join(random.choices(_LETTERS_AND_DIGITS, k=6))
+
 
 class LinearFace(traitlets.HasTraits):
-    rect_angle = traitlets.Float()
+    angle = traitlets.Float()
     callback = traitlets.Any()
-    
-    def __init__(self, x1=0, y1=0, x2=1, y2=1, length=10, width=2):
-        self.p1 = Point(0,0, description="p0")
-        self.p2 = Point(0,0, description="p1")
-        self.length = widgets.Text(description="Length", value=str(length))
+
+    def __init__(self, x1=0, y1=0, x2=1, y2=1, height=10, width=2):
+        self.p1 = Point(0, 0, description="p0")
+        self.p2 = Point(0, 0, description="p1")
+        self.height = widgets.Text(description="height", value=str(height))
         self.width = widgets.Text(description="Width", value=str(width))
         # self.midpoint = some_math.Point(*some_math.midpoint(self.p1, self.p2), description="midpoint")
         # self.rect_origin = some_math.Point(0,0, description="rect origin")
-        self.midpoint = Point(0,0, description="midpoint", disabled=True)
-        self.rect_origin = Point(0,0, description="rect origin", disabled=True)
-        self.widg_rect_angle = widgets.Text("", description="rot (deg)", disabled=True)
+        self.midpoint = Point(0, 0, description="midpoint", disabled=True)
+        self.rect_origin = Point(0, 0, description="rect origin", disabled=True)
+        self.text_angle = widgets.Text("", description="rot (deg)")
         self.color_picker = widgets.ColorPicker(
-            description='line',
-            value=random_hex_color()
+            description="line", value=random_hex_color()
         )
         self.name = widgets.Text(description="name", value=word_pair(), disabled=True)
-        
-        recompute = [self.p1, self.p2, self.length, self.width]
+
+        recompute = [self.p1, self.p2, self.height, self.width]
         self.redraw = recompute + [self.color_picker]
-        self.derived = [widgets.Label("Derived"), self.midpoint, self.rect_origin, self.widg_rect_angle]
-        self.layout = widgets.HBox([
-            widgets.VBox([widgets.Label("Inputs")] + self.redraw + [self.name]),
-            widgets.VBox(self.derived),
-        ])
-        
+        self.derived = [
+            widgets.Label("Derived"),
+            self.midpoint,
+            self.rect_origin,
+            self.text_angle,
+        ]
+        self.layout = widgets.HBox(
+            [
+                widgets.VBox([widgets.Label("Inputs")] + self.redraw + [self.name]),
+                widgets.VBox(self.derived),
+            ]
+        )
+
         # wire up interactivity
         for widg in recompute:
-            widg.observe(self._update_derived, names='value')
-            
-        self.observe(self._update_rect_angle, names=['rect_angle'])
-            
+            widg.observe(self._update_derived, names="value")
+
+        self.observe(self._update_angle, names=["text_angle"])
+        self.text_angle.observe(self._update_angle, names=["value"])
+
         self.p1.x, self.p1.y = x1, y1
         self.p2.x, self.p2.y = x2, y2
-        
-    def _update_derived(self,change):
+
+    def _update_derived(self, change):
         print(change)
         self.midpoint.x, self.midpoint.y = midpoint(self.p1, self.p2)
         # origin of rectangle should be "upstream" of the midpoint, so we reverse the point vector
         r, th = to_polar(self.p2, self.p1)
         self.rect_angle = th * 180 / np.pi
         x, y = distance_polar(self.midpoint, float(self.width.value), th)
-        self.rect_origin.x, self.rect_origin.y = x,y
+        self.rect_origin.x, self.rect_origin.y = x, y
         if self.callback is not None:
             self.callback(self)
-    
-    def _update_rect_angle(self, change):
-        self.widg_rect_angle.value = f'{self.rect_angle}'
-    
-        
+
+    def _update_angle(self, change):
+        if change["name"] == "value":
+            self.angle = float(self.rect_angle.value)
+        elif change["name"] == "angle":
+            self.rect_angle.value = str(self.angle)
+        else:
+            print(f"not sure what to do with {change}")
+
+
 #     def _update_rectangle(self, change):
 #         x,y = self._compute_midpoint()
 #         width = float(self.width.value)
 #         length = float(self.length.value)
-#         p1 = 
-        
+#         p1 =
+
 
 ## point functions
 def to_polar(p1: Point, p2: Point) -> Tuple[float, float]:
@@ -113,26 +163,75 @@ def to_polar(p1: Point, p2: Point) -> Tuple[float, float]:
     theta = angle(p1, p2)
     return r, theta
 
+
 def distance(p1: Point, p2: Point) -> float:
-    return spatial.distance.euclidean(
-        (p1.x, p1.y), (p2.x, p2.y))
+    return spatial.distance.euclidean((p1.x, p1.y), (p2.x, p2.y))
+
 
 def angle(p1: Point, p2: Point) -> float:
-    th = np.arctan2((p2.y-p1.y), (p2.x-p1.x))
+    th = np.arctan2((p2.y - p1.y), (p2.x - p1.x))
     return th
+
 
 def angle_degrees(p1: Point, p2: Point) -> float:
     a = angle(p1, p2)
     return a * 180 / np.pi
-    
 
-def midpoint(p1: Point, p2: Point) -> Tuple[float, float]: 
+
+def midpoint(p1: Point, p2: Point) -> Tuple[float, float]:
     xm = (p1.x + p2.x) / 2
     ym = (p1.y + p2.y) / 2
-    return (xm,ym)
+    return (xm, ym)
 
 
 def distance_polar(p: Point, r: float, th: float) -> Tuple[float, float]:
     x = r * np.cos(th) + p.x
     y = r * np.sin(th) + p.y
     return x, y
+
+
+def mpl_rect_patch(axes, x, y, width, height, rot, color=None):
+    """Helper function to generate a rectangle patch
+
+    Parameters
+    ----------
+    axes: mpl axes object
+    `x` coord in data space
+    `y` coord in data space
+    `width` of rectangle
+    `height` of rectangle
+    `rot`. angle, in degrees, of rotation around (x,y)
+    `color` is the color for the patch
+
+    Returns
+    -------
+    patch: generated patch
+    """
+    if color is None:
+        color = random_hex_color()
+    patch = patches.Rectangle(
+        xy=(x, y), width=width, height=height, color=color, alpha=0.5
+    )
+    t2 = mpl.transforms.Affine2D().rotate_deg_around(x, y, rot) + axes.transData
+    patch.set_transform(t2)
+    return patch
+
+
+def imshow_with_patches(ax, data, patches: dict):
+    try:
+        im = ax.images[-1]
+    except IndexError:
+        print("No image currently show. calling ax.imshow")
+        im = ax.imshow(data)
+    else:
+        im.set_data(data)
+
+    print(f"Removing {len(ax.patches)} existing patches")
+
+    for existing_patch in ax.patches:
+        existing_patch.remove()
+    added_patches = {}
+    for patch_name, patch in patches.items():
+        print(f"Adding patch for {patch_name}")
+        added_patches[patch_name] = ax.add_patch(patch)
+    return im, added_patches
--- some_math/test_math.py
+++ some_math/test_math.py
@@ -1,15 +1,24 @@
-from . import LinearFace, Point, distance, angle_degrees, to_polar, midpoint, distance_polar
+from . import (
+    LinearFace,
+    Point,
+    distance,
+    angle_degrees,
+    to_polar,
+    midpoint,
+    distance_polar,
+)
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
 
 def test_distance():
-    x0,y0 = 0,0
-    x1,y1 = 1,1
-    p0 = Point(x0,y0)
-    p1 = Point(x1,y1)
+    x0, y0 = 0, 0
+    x1, y1 = 1, 1
+    p0 = Point(x0, y0)
+    p1 = Point(x1, y1)
 
-    np_dist = np.sqrt((p1.x-p0.x)**2 + (p1.y-p0.y)**2)
+    np_dist = np.sqrt((p1.x - p0.x) ** 2 + (p1.y - p0.y) ** 2)
     assert distance(p0, p1) == np_dist
 
 
@@ -25,18 +34,19 @@ def test_angle():
     assert angle_degrees(p0, p3) == pytest.approx(-135)
     assert angle_degrees(p0, p4) == pytest.approx(-45)
 
+
 def test_to_polar():
-    p0 = Point(0,0)
-    p1 = Point(1,1)
+    p0 = Point(0, 0)
+    p1 = Point(1, 1)
     r, th = to_polar(p0, p1)
     assert r == np.sqrt(2)
     assert th == np.pi / 4
 
 
 def test_widget():
-    p0 = Point(0,0)
+    p0 = Point(0, 0)
     p0.x = 1
-    
+
     assert p0.value == "1.0,0.0"
 
     p0.value = "2,3"
@@ -45,14 +55,14 @@ def test_widget():
 
 
 def test_midpoint():
-    p0 = Point(0,0)
-    p1 = Point(4,2)
+    p0 = Point(0, 0)
+    p1 = Point(4, 2)
 
-    x1,y1 = midpoint(p0, p1)
+    x1, y1 = midpoint(p0, p1)
     assert x1 == pytest.approx(2)
     assert y1 == pytest.approx(1)
 
-    x2,y2 = midpoint(p1, p0)
+    x2, y2 = midpoint(p1, p0)
     assert x2 == pytest.approx(2)
     assert y2 == pytest.approx(1)
 
@@ -62,31 +72,36 @@ def test_distance_polar():
     r = np.sqrt(2)
     th = np.pi / 4
 
-    x, y = distance_polar(p0, np.sqrt(2), np.pi/4)
+    x, y = distance_polar(p0, np.sqrt(2), np.pi / 4)
     assert x == pytest.approx(1)
     assert y == pytest.approx(1)
 
-    x, y = distance_polar(p0, np.sqrt(2), np.pi*3/4)
+    x, y = distance_polar(p0, np.sqrt(2), np.pi * 3 / 4)
     assert x == pytest.approx(-1)
     assert y == pytest.approx(1)
 
-    x, y = distance_polar(p0, r, -np.pi/4)
+    x, y = distance_polar(p0, r, -np.pi / 4)
     assert x == pytest.approx(1)
     assert y == pytest.approx(-1)
 
-    x, y = distance_polar(p0, r, -np.pi*3/4)
+    x, y = distance_polar(p0, r, -np.pi * 3 / 4)
     assert x == pytest.approx(-1)
     assert y == pytest.approx(-1)
 
 
 def test_line_thing():
-    x0, y0 = 0,0
-    x1,y1 = 2,4
-    width = np.sqrt((x1/2)**2 + (y1/2)**2)
+    x0, y0 = 0, 0
+    x1, y1 = 2, 4
+    width = np.sqrt((x1 / 2) ** 2 + (y1 / 2) ** 2)
     length = 10
     lf = LinearFace(x0, y0, x1, y1, length=length, width=width)
 
-    assert lf.midpoint.x == pytest.approx(x1/2)
-    assert lf.midpoint.y == pytest.approx(y1/2)
+    assert lf.midpoint.x == pytest.approx(x1 / 2)
+    assert lf.midpoint.y == pytest.approx(y1 / 2)
     assert lf.rect_origin.x == pytest.approx(0)
     assert lf.rect_origin.y == pytest.approx(0)
+
+
+def test_imshow_with_patches():
+    fig, ax = plt.subplots()
+    
